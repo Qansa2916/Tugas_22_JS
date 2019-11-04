eefd045d4bc2e1ef1933707a9413f1046875a79b
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 10:49:27 +0700

    Tugas_22_JS

---
+++ Tugas-22.js
@@ -0,0 +1,7 @@
+function tugas22() {
+    var x = ["Saya", "ingin", "Belajar", "Bersama"];
+    x.forEach(function (satu, dua) {
+        console.log("Item : ", satu, "index ke :", dua);
+    })
+}
+tugas22();
